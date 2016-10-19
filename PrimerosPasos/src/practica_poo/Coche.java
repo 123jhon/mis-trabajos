eb3753f0f2f0a3c1bae5f53fc6b6d3e4cd8fe26c
123jhon <jajoya17@misena.edu.co>
2016-10-18 21:25:37 -0500

    practica con clases y metodos sobre escritos

---
+++ PrimerosPasos/src/practica_poo/Coche.java
@@ -0,0 +1,84 @@
+package practica_poo;
+/** * * @author jhone */
+public class Coche {
+    private int ruedas;
+    private int largo;
+    private int ancho;
+    private int motor;
+    private int peso;
+    private String Marca="BMW";
+    private String Descripcion;
+    public Coche(){}
+    public Coche(int ancho,int largo,int motor,int ruedas,int peso,String Descripcion){
+        this.ancho=ancho;
+        this.largo=largo;
+        this.motor=motor;
+        this.ruedas=ruedas;
+        this.peso=peso;
+        this.Descripcion=Descripcion;
+    }
+
+    public int getRuedas() {
+        return ruedas;
+    }
+
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public int getMotor() {
+        return motor;
+    }
+
+    public void setMotor(int motor) {
+        this.motor = motor;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+   
+    @Override
+    public String toString() {
+        return "LA "+Marca +Descripcion+ " \nruedas de tipo A CANT-> " + ruedas
+                + "\nlargo->" + largo + "cn\nancho->" 
+                + ancho + "cm\n motor-> " + motor + "cn\npeso->" + peso +"KL";
+    }
+    
+}
---
+++ PrimerosPasos/src/practica_poo/Uso_Coche.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_poo;
+
+/**
+ *
+ * @author jhone
+ */
+public class Uso_Coche {
+    public static void main(String[] caramelo){
+            //instanciando una clase
+        Coche micarrito=new Coche(300,120, 300,4,12,"se complace en mostrar la"
+                + "nueva linea de autos inteligentes con estas carateristicas"
+                + ":");//Ejemplar de clase
+        //ejemplarizando una clase
+        System.out.println(micarrito.toString());
+    
+    }
+}
