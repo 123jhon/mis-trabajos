018f2b43f1ac222caf6f6f1c30eadc306c85c0e3
123jhon <jajoya17@misena.edu.co>
2016-10-30 21:52:25 -0500

    se agregan al proyecto login la clase que dara el manejo de los datos a la tabla user en la base de prueba almacenada en mysql cierre por sueño

--- Loguin/src/pra/modelo/Conexion.java
+++ Loguin/src/pra/modelo/Conexion.java
@@ -5,29 +5,49 @@
  */
 package pra.modelo;
 import java.sql.*;
+import java.lang.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author jhone
  */
 public class Conexion {
-        
-    //creacion de constructor
-    public Conexion(){}
-    
-    public Connection getConexion(){
-        Connection con=null;
-        //manejo de excepciones en el momento
-        //al hacer conexion a la bd
-        try {
-            /*Instanciar el driver de db*/
-            /*se encuentra en la ibreria jdbc */
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
             //Conexion a la base de datos
             //PRIMERO EL URL DE conexion el segundo el cliente en este caso 
             //root de la bd
             // en el otro la contraseña
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_prueba","root","nbuser");
-        } catch (Exception e) {
+    public final static String SERVER_NAME = "localhost";
+    public final static String SERVER_PORT = "3306";
+    public final static String DB_NAME = "db_session";
+    public final static String DB_USER = "root";
+    public final static String DB_PASS = "nbuser"; 
+    //creacion de constructor
+    public Conexion(){}
+    
+        private static Conexion singleton;
+        private Connection con;
+    
+    public Connection getConexion(){
+        //manejo de excepciones en el momento
+        //al hacer conexion a la bd
+        try {
+            /*Instanciar el driver de db*/ /*se encuentra en la ibreria jdbc */
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String url = "jdbc:mysql://" + SERVER_NAME + ":" + SERVER_PORT + "/" + DB_NAME; 
+            this.con = DriverManager.getConnection(url, DB_USER, DB_PASS);
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showInputDialog("Verifique el conector jdbc");
+        } catch (SQLException ex) {
+            if(ex.getErrorCode() == 1049){
+                JOptionPane.showInputDialog(ex.getErrorCode() +": Verifique el nombre de la base de datos.");
+            } else if(ex.getErrorCode() == 1045){
+                JOptionPane.showInputDialog(ex.getErrorCode() +": Verifique el usuario de la base de datos.");
+            } else{
+                JOptionPane.showInputDialog(ex.getErrorCode() +": Verifique la concexion al servidor y datos de conexión.");
+            }
+        }
+        catch (IllegalAccessException | InstantiationException II) {
+            JOptionPane.showInputDialog("Caray hiciste algo mal");
         }
  
         return con;
---
+++ Loguin/src/pra/modelo/Empleado.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pra.modelo;
+
+/**
+ *
+ * @author jhone
+ */
+public class Empleado {
+    private int log_dni;
+    private String log_pass;
+    private String log_primerApellido;
+    private String log_segApellido;
+    private String log_priNombre;
+    private String log_segNombre;
+    private String log_Privilegios;
+    public Empleado(){
+        this.log_dni=0;
+        this.log_primerApellido="";
+        this.log_segApellido="";
+        this.log_priNombre="";
+        this.log_segNombre="";
+        this.log_Privilegios="";
+        
+    }
+
+    public int getLog_dni() {
+        return log_dni;
+    }
+
+    public void setLog_dni(int log_dni) {
+        this.log_dni = log_dni;
+    }
+
+    public String getLog_pass() {
+        return log_pass;
+    }
+
+    public void setLog_pass(String log_pass) {
+        this.log_pass = log_pass;
+    }
+
+    public String getLog_primerApellido() {
+        return log_primerApellido;
+    }
+
+    public void setLog_primerApellido(String log_primerApellido) {
+        this.log_primerApellido = log_primerApellido;
+    }
+
+    public String getLog_segApellido() {
+        return log_segApellido;
+    }
+
+    public void setLog_segApellido(String log_segApellido) {
+        this.log_segApellido = log_segApellido;
+    }
+
+    public String getLog_priNombre() {
+        return log_priNombre;
+    }
+
+    public void setLog_priNombre(String log_priNombre) {
+        this.log_priNombre = log_priNombre;
+    }
+
+    public String getLog_segNombre() {
+        return log_segNombre;
+    }
+
+    public void setLog_segNombre(String log_segNombre) {
+        this.log_segNombre = log_segNombre;
+    }
+
+    public String getLog_Privilegios() {
+        return log_Privilegios;
+    }
+
+    public void setLog_Privilegios(String log_Privilegios) {
+        this.log_Privilegios = log_Privilegios;
+    }
+    
+}
